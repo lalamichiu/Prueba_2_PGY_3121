247045f7dd011e88da1215efc1c897a534b6ab5e
PabloYovera <90583919+PabloYovera@users.noreply.github.com>
2022-05-16 21:35:26 -0400

    Add files via upload

---
+++ recarga.js
@@ -0,0 +1,21 @@
+const iFrameNosotros = '<iframe class="ifForm" src="Nosotros.html" scrolling="no" frameborder="0"></iframe>'
+const iFrameTienda = '<iframe class="ifForm" src="Tienda.html" scrolling="no" frameborder="0"></iframe>'
+const iFrameDonaciones = '<iframe class="ifForm" src="donaciones.html" scrolling="no" frameborder="0"></iframe>'
+const iFrameContacto = '<iframe class="ifForm" src="Contacto.html" scrolling="no" frameborder="0"></iframe>'
+
+const mostrarNosotros = ()=>{
+    Carga.innerHTML=iFrameNosotros
+}
+const mostrarTienda = ()=>{
+    Carga.innerHTML=iFrameTienda
+}
+const mostrarDonaciones = ()=>{
+    Carga.innerHTML=iFrameDonaciones
+}
+const mostrarContacto = ()=>{
+    Carga.innerHTML=iFrameContacto
+}
+
+// fetch('https://reqres.in/api/users?page=2')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
